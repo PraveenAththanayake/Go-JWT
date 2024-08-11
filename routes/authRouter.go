a35985bee58464b3636dc791f0bd7e4c7261944b
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-08-12 00:57:54 +0530

    fix: Fixed the imported package issue in authRouter.go file

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	controller "github.com/PraveenAththanayake/Go-JWT/controller"
+	controller "github.com/PraveenAththanayake/Go-JWT/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/signup", controller.SignUp())
 	incomingRoutes.POST("users/login", controller.Login())
 }
